2b4468015c631161a801dc9dfa25592439bc015f
wertqyuio <ultimatenarutofan@gmail.com>
2020-02-20 11:07:54 -0800

    All basic functionality ready

--- src/PortfolioPage.js
+++ src/PortfolioPage.js
@@ -7,12 +7,16 @@ export default class PortfolioPage extends Component {
   render() {
     return (
       <span>
-        <div>
-          <span>Jobly</span>
-          <span>Center for Bridge Website</span>
-          <span>Yahtzee</span>
-          <span>Hangman</span>
-          <span>Connect 4</span>
+        <div className="Portfolio-Image">
+          <ul>
+            <p className="Titleist">Selection of Projects</p>
+            <li>Jobly</li>
+            <li>Center for Bridge Education</li>
+            <li>Warbler</li>
+            <li>Adoption Agency</li>
+            <li>Connect 4</li>
+            <li>Yahtzee</li>
+          </ul>
         </div>
       </span>
     )
