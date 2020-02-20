bfa52dad3395f1c3edeb6996de9678d1411c5e51
wertqyuio <ultimatenarutofan@gmail.com>
2020-02-20 09:08:30 -0800

    Added bridge content, note: change z-idx?

--- src/BridgePage.js
+++ src/BridgePage.js
@@ -10,7 +10,15 @@ export default class BridgePage extends Component {
         <div className="Reading-Image">
             <p className="Titleist">Personal Bridge Links</p>
             <span><a href="www.google.com">
-              My Bridge Power Rating</a></span>
+              National Championship Win</a></span>
+            <span><a href="www.google.com">
+              Bridge Winners Link</a></span>
+            <span><a href="www.google.com">
+              Bridge Power Rating</a></span>
+            <span><a href="www.google.com">
+              Tournament Results</a></span>
+            <span><a href="www.google.com">
+              Club Results</a></span>
         </div>
       </span>
     )
