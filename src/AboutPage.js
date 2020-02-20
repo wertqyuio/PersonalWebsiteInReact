d55bf2b116305bc2f01b9058cf93dd8c7dc4352b
wertqyuio <ultimatenarutofan@gmail.com>
2020-02-20 10:59:08 -0800

    Added about page content

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -7,8 +7,12 @@ export default class AboutPage extends Component {
   render() {
     return (
       <span>
-        <div className="Reading-Image">
-          <span>My name is Chris Chen and I'm currently a contract software for a 501(c)(3) charity, Center for Bridge Education. I work on bridge related educational projects. In my spare time, I enjoy playing bridge, and reading.</span>
+        <div className="About-Image">
+          <p>My name is Chris Chen and I'm currently a contract
+             software engineer for a 501(c)(3) charity, 
+            Center for Bridge Education. I work on bridge related 
+            educational projects. In my spare time, I enjoy playing bridge, 
+            and reading.</p>
         </div>
       </span>
     )
