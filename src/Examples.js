3b0f73e53124fffc8963ed105f58629b6b632ffd
wertqyuio <ultimatenarutofan@gmail.com>
2020-03-09 16:48:16 -0700

    Created buttons describing every portfolio project

--- src/Examples.js
+++ src/Examples.js
@@ -8,12 +8,12 @@ const Example = (props) => {
 
   return (
     <div>
-      <Button id="Popover1" type="button">
-        Launch Popover
+      <Button id={"Popover" + props.id} type="button">
+        {props.title}
       </Button>
-      <Popover placement="bottom" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
-        <PopoverHeader>Popover Title</PopoverHeader>
-        <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
+      <Popover trigger="focus" placement="bottom" isOpen={popoverOpen} target={"Popover" + props.id} toggle={toggle}>
+        <PopoverHeader>{props.title}</PopoverHeader>
+        <PopoverBody>{props.description}</PopoverBody>
       </Popover>
     </div>
   );
--- src/PortfolioPage.js
+++ src/PortfolioPage.js
@@ -1,4 +1,5 @@
 import React, { Component, useState } from 'react';
+import Example from './Examples';
 import Center from './Center';
 import ExternalLinks from './ExternalLinks';
 import "./PortfolioPage.css"
@@ -10,15 +11,31 @@ export default class PortfolioPage extends Component {
         <div className="Portfolio-Image">
           <ul>
             <p className="Titleist">Selection of Projects</p>
-            <li>Jobly</li>
-            <li>Center for Bridge Education</li>
-            <li>Warbler</li>
-            <li>Adoption Agency</li>
-            <li>Connect 4</li>
-            <li>Yahtzee</li>
+            <Example title="Jobly" id="jobly"
+              description={`Full stack application for job posting.
+              Front end created in React with a frontend API for get
+              requests for job requests and job requests by companies.
+              Back end created with Node to connect with frontend API.`}>
+            </Example>
+            <Example title="Warbler" id="warbler"
+              description={`Twitter-like clone with back-end done in Flask
+            and Python. Includes user log-in features.`}></Example>
+            <Example title="Adoption Agency" id="adoptionagency"
+              description={`Application created with Python and Flask.
+            Designed to make pet adoption as simple and easy.`}></Example>
+            <Example title="Connect 4" id="connect4"
+              description={`Application built in classic HTML, CSS, JS
+            to play Connect 4`}></Example>
+            <Example title="Yahtzee" id="yahtzee"
+              description={`Play the game Yahtzee built with React.`}></Example>
+            <Example title="Center for Bridge Education" id="cbe"
+              description={`Website for Center for Bridge Education.
+            Designed for easier content upload with third-party platform.
+            Webpages include educational bridge resources, calendar,
+            donations portal, and merchandising page.`}></Example>
           </ul>
         </div>
-      </span>
+      </span >
     )
   }
 }
